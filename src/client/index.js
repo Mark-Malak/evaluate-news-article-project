87318609fad882a43a0fdd30672e459c7ea3f471
Mark-Malak <markmalak71@gmail.com>
2021-08-12 02:18:21 +0200

    final touches

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,6 @@
 import handleSubmit from './js/formHandler'
 
-import './styles/base.scss'
+import './styles/main.scss'
 import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
